33e70b1509d784c8ecf306fa6f92a03b95d1820e
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-30 09:57:09 -0400

    added engineer and intern prompts

---
+++ app-starter.js
@@ -0,0 +1,173 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+//__ is node global variable . will give you absolute path to current directory. "output will be your created sub-folder name"
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+//Using the output directory created it is going to create the file "team.html"
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// Rendering function
+const render = require("./lib/page-template.js");
+
+const teamMembers = [];
+// Create an id array to store the ids.
+// This array will be used to check the potential duplicate id newly entered by user
+const idArray = [];
+
+function appMenu() {
+  //Create the manager first
+  function createManager() {
+    console.log("Please build your team");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the manager's name?",
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the manager's ID?",
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the manager's email?",
+        },
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is the manager's officer number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+  }
+  //Begin to create the team
+  function createTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "memberChoice",
+          message: "Which type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        switch (userChoice.memberChoice) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
+  }
+  //Add an engineer
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's ID?",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email?",
+        },
+        {
+          type: "input",
+          name: "GitHubUser",
+          message: "What is the engineer's Github username?",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.GitHubUser
+        );
+        teamMembers.push(engineer);
+        idArray.push(answers.engineerId);
+        createTeam();
+      });
+  }
+  //Add an intern
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the intern's name?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the intern's ID?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the intern's email?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What school did the intern go to?",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        idArray.push(answers.internId);
+        createTeam();
+      });
+  }
+
+  function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
+
+  createManager();
+}
+
+appMenu();
